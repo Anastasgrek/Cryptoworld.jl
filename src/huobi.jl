f562a9c38157ebad639467cb165479958b6b9d65
Anastasgrek <akonstantinidi3@gmail.com>
2020-10-11 16:08:22 +0300

    create huobi lib

--- src/huobi.jl
+++ src/huobi.jl
@@ -7,7 +7,6 @@ using Base64
 using Dates
 using Printf
 using DataStructures
-using StringEncodings
 
 global const BASE_URL = "https://api.huobi.pro"
 
@@ -15,20 +14,118 @@ publickey() = ENV["HUOBI-PUBLIC-KEY"]
 secretkey() = ENV["HUOBI-SECRET-KEY"]
 
 
-function signature(secret::String, query::String)
+function signature(
+    method::AbstractString,
+    host::AbstractString,
+    path::AbstractString,
+    query::AbstractDict,
+    secret::AbstractString
+)
+    pairs = join(string.(keys(query),"=", HTTP.URIs.escapeuri.(values(query))), "&")
+    str_query = join([method, host, path, pairs], "\n")
 
-    sha256 = HMACState("sha256", secret)
-    Nettle.update!(sha256, query)
+    hmac = HMACState("sha256", secret)
+    Nettle.update!(hmac, str_query)
 
-    return base64encode(Nettle.hexdigest!(sha256))
+    Base64.base64encode(Nettle.digest!(hmac))
 end
 
 
 
+function request(
+    method::String,
+    path::String,
+    query::AbstractDict,
+    public::Union{Missing,String},
+    secret::Union{Missing,String},
+)
+
+    @debug "request" method path (;zip(keys(query) .|> Symbol, values(query))...)...
+
+    !(method ∈ ["GET", "POST"]) &&
+        error("unknown method $method")
+
+    timestamp = Dates.format(now(UTC), "yyyy-mm-ddTHH:MM:SS")
+
+    params = [
+        "AccessKeyId" => public,
+        "SignatureMethod" => "HmacSHA256",
+        "SignatureVersion" => 2,
+        "Timestamp" => timestamp
+    ] |> OrderedDict
+
+    path = "/" * path
+    host = HTTP.URI(BASE_URL).host
+    url = string(BASE_URL, path)
+
+    isequal(method, "GET") &&
+        merge!(params, query)
+
+    params["Signature"] =
+        signature(method, host, path, params, secret)
+
+    response = if method == "GET"
+        HTTP.request(method, url, query=params, status_exception=false)
+    else
+        jsn = isempty(query) ? "" : JSON.json(query)
+        HTTP.request(method, url, query=params, jsn, status_exception=false)
+    end
+
+    json = JSON.parse(String(response.body))
+
+    if haskey(json, "status")
+        if json["status"] == "error"
+            error(string(
+                json["err-msg"], " ",
+                JSON.json(query)
+            ))
+        end
+    end
+
+    if haskey(json, "code")
+        if json["code"] != 200
+            error(string(
+                JSON.json(query)
+            ))
+        end
+    end
+
+    @debug "response" method path (;zip(keys(json) .|> Symbol, values(json))...)...
+
+    return json
+end
+
+request(
+    method::String,
+    path::String,
+    query::AbstractDict = OrderedDict()
+) = request(method, path, query, publickey(), secretkey())
+
+#Signed requests
+
+accounts() =
+    request("GET", "v1/account/accounts", OrderedDict())
+
+function balances(type::String)
+    response = accounts()
+    filter!(x-> x["type"] == "$type", response["data"])
+    account_id = map(x -> x["id"], response["data"])[1]
+    request("GET", "v1/account/accounts/$account_id/balance", OrderedDict())
+end
+
+function balance(currency::String, type::String)
+    response = balances("$type")
+    for cur in response["data"]["list"]
+        if currency == cur["currency"]
+            return cur["balance"]
+        else
+            nothing
+        end
+    end
+end
 
 
 #Public requests
-
 function details()
     endpoint = "/v1/common/symbols"
     response = HTTP.request("GET", string(BASE_URL, endpoint))
@@ -71,6 +168,7 @@ function stats_24hr(symbol::String)
     return jsn["tick"]["vol"]
 end
 
+
 function order_book(symbol::String, type::String="step0")
     _path = "market/depth"
     url = join([BASE_URL, _path], "/")
@@ -100,22 +198,4 @@ function historical_trades(symbol::String, size::Int)
     return json["data"]
 end
 
-#Signed requests
-
-# function accounts()
-_path = "v1/account/accounts"
-
-query = OrderedDict("AccessKeyId" => publickey(),
-                    "SignatureMethod" => "HmacSHA256",
-                    "SignatureVersion" => "2",
-                    "Timestamp" => HTTP.escapeuri(string(round(DateTime(now(UTC)), Dates.Second(1)))))
-
-pre_sigened = "GET\napi.huobi.pro\n/v1/account/accounts\n"
-query = join(string.(keys(query), "=" , values(query)), "&")
-params = string(pre_sigened, join(string.(keys(query), "=" , values(query)), "&"))
-
-res = HTTP.request("GET", string(BASE_URL, "/", _path, "?", query, "&Signature=", signature(secretkey(), params)))
-json = JSON.parse(String(res.body))
-# end
 end
-round(DateTime(now(UTC)), Dates.Second(1))
